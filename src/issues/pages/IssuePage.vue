0d5d40c091c9fc01577cc3178d12b373e9f1733e
Jorge Sone <jorge.sone@dataddo.com>
2022-12-11 12:06:49 +0100

    updated IssuePage

--- src/issues/pages/IssuePage.vue
+++ src/issues/pages/IssuePage.vue
@@ -1,8 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import LoaderSpinner from '../../shared/components/LoaderSpinner.vue';
+import IssueCard from '../components/issues-list/IssueCard.vue';
+
+const route = useRoute();
+
+const { id } = route.params;
+</script>
 
 <template>
-  <div>
-    <h1>Issue Page</h1>
+  <LoaderSpinner color="white" />
+
+  <RouterLink class="text-white" to="/">Go to issue</RouterLink>
+  <!-- Header -->
+  <IssueCard />
+  <!-- Comments -->
+  <LoaderSpinner
+    color="white"
+    :thickness="1"
+    size="1.5rem"
+    :show-text="false"
+  />
+
+  <div class="column">
+    <span class="text-h3 q-mb-md"> Comments (5) </span>
+    <IssueCard v-for="comment of 5" :key="comment" />
   </div>
 </template>
 
